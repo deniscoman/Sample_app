d7f00f1e3defa682f2ffb180e3b34f441d9b0611
Denis Coman <denis.coman13@gmail.com>
2017-07-14 14:22:19 +0300

    Set email to a lower-case version before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   validates :name, presence: true, length: {maximum:50}
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum:255}, format: {with: VALID_EMAIL_REGEXP} , uniqueness: {case_sensitive: false}
+  validates :email, presence: true, length: {maximum:255},
+            format: {with: VALID_EMAIL_REGEXP} ,
+            uniqueness: {case_sensitive: false}
 end
