a06e0ec7d08c2bec0b61c269457fcc0e406b4d19
Denis Coman <denis.coman13@gmail.com>
2017-07-26 15:34:25 +0300

    Add account activation resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Denis Coman",
              email: "denis@denis.com",
              password:              "123456",
              password_confirmation: "123456",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
