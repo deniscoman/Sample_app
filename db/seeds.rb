df3b1f19f1cf0fc9698a57f074cf6b4b8be7e01b
Denis Coman <denis.coman13@gmail.com>
2017-07-26 12:05:11 +0300

    Add administrative user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  "Denis Coman",
+             email: "denis@denis.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
